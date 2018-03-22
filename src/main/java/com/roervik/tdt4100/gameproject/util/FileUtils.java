fd1a7d5d92d5cf9e1f1bc610fab693385a168953
potrik98 <potrik98@gmail.com>
2018-03-22 23:19:27 +0100

    Add FileUtils and ShaderLoader

---
+++ src/main/java/com/roervik/tdt4100/gameproject/util/FileUtils.java
@@ -0,0 +1,18 @@
+package com.roervik.tdt4100.gameproject.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+    public static String readInputStream(final InputStream inputStream) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        final StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        return out.toString();
+    }
+}
