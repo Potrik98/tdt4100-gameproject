694db81fa426d3de0dc49739335b141f1cd2466b
potrik98 <potrik98@gmail.com>
2018-04-10 23:57:50 +0200

    Add readAllLines to FileUtils

--- src/main/java/com/roervik/tdt4100/gameproject/util/FileUtils.java
+++ src/main/java/com/roervik/tdt4100/gameproject/util/FileUtils.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtils {
     public static String readInputStream(final InputStream inputStream) throws IOException {
@@ -15,5 +17,24 @@ public class FileUtils {
             out.append("\n");
         }
         return out.toString();
+    }
+
+    public static List<String> readAllLines(final String resourcePath)  {
+        return readAllLines(ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(resourcePath));
+    }
+
+    public static List<String> readAllLines(final InputStream inputStream) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unable to read resource " + inputStream.toString());
+        }
+        return list;
     }
 }
